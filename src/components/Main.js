422b9a144ec71e22ca2823432963894142ca34e0
Ntando Molose <ntandomolose@gmail.com>
2026-05-27 07:35:48 +0200

    Routed main and nav pages

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './HomePage';
+import BookingPage from './BookingPage';
 
 const Main = () => {
   return (
-    <main>
-      <h1>Welcome to Little Lemon</h1>
-      {/* Future routing and page components will go here */}
-    </main>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/booking" element={<BookingPage />} />
+    </Routes>
   );
 };
 
