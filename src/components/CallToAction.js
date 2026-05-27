828918c7f56df54ce651088f03a60ae23c473560
Ntando Molose <ntandomolose@gmail.com>
2026-05-27 07:30:44 +0200

    Added CallToAction and Specials components

---
+++ src/components/CallToAction.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const CallToAction = () => {
+  return (
+    <section className="hero">
+      <div className="hero-content">
+        <h1>Little Lemon</h1>
+        <h2>Chicago</h2>
+        <p>
+          We are a family owned Mediterranean restaurant, focused on traditional
+          recipes served with a modern twist.
+        </p>
+        <button aria-label="Reserve a Table">Reserve a Table</button>
+      </div>
+    </section>
+  );
+};
+
+export default CallToAction;
---
+++ src/components/Specials.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Specials = () => {
+  return (
+    <section className="specials">
+      <div className="specials-header">
+        <h2>This week's specials!</h2>
+        <button>Online Menu</button>
+      </div>
+      {/* You will map through actual data here later */}
+      <p>[Special Item Cards will go here]</p>
+    </section>
+  );
+};
+
+export default Specials;
